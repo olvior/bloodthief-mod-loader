8c349938bdfcb71d6a1e2d45df1fe9b45c730e2f
Olvior <olvioro@gmail.com>
2024-08-01 19:22:45 +0200

    Fixed path

--- example_mods/mods-unpacked/olvior-TestLevel/mod_main.gd
+++ example_mods/mods-unpacked/olvior-TestLevel/mod_main.gd
@@ -6,4 +6,4 @@ func init():
 
 func _ready():
 	var level_helper = ModLoader.all_mods["olvior-LevelHelper"]
-	level_helper.create_level("Supercoollevel", 1, "res://mods-unpacked/olvior-TestLevel/testroom.tscn", "Super cool level", "supercoollevelboard", [10, 20, 30], true)
+	level_helper.create_level("Supercoollevel", 1, "res://olvior-TestLevel/testroom.tscn", "Super cool level", "supercoollevelboard", [10, 20, 30], true)
