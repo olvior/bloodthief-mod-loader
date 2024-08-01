7ed60cc84ed1d2272f8825a740eee43d5fa3ff47
Olvior <olvioro@gmail.com>
2024-08-01 09:31:06 +0200

    Created example mod that uses the helper mod

---
+++ example_mods/mods-unpacked/olvior-TestLevel/mod_main.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func init():
+	print("Test level mod loaded")
+
+func _ready():
+	var level_helper = ModLoader.all_mods["olvior-LevelHelper"]
+	level_helper.create_level("Supercoollevel", 1, "res://mods-unpacked/olvior-TestLevel/testroom.tscn", "Super cool level", "supercoollevelboard", [10, 20, 30], true)
