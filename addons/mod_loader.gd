5a6aa55258472fc213d3d013c34eb7dd6dfd0502
Olvior <olvioro@gmail.com>
2024-08-06 22:32:36 +0200

    Added logging capabilities

--- addons/mod_loader.gd
+++ addons/mod_loader.gd
@@ -3,18 +3,34 @@ extends Node
 var all_mods = {}
 
 func _ready():
-	print("Starting up the mod loader")
+	init_logs()
+	
+	mod_log("Starting up the mod loader")
+	mod_log('')
+
+	start_mod_loading()
+
+
+
+func start_mod_loading():
 	# open the mods folder
 	var mods_folder_path = OS.get_executable_path().get_base_dir() + "/mods-unpacked"
-	print(mods_folder_path)
 	var mods_folder = DirAccess.open(mods_folder_path)
-	print(mods_folder)
+	
+	mod_log("Mods unpacked path: " + mods_folder_path)
+
+	if not mods_folder:
+		mod_log("Failed to open mods-unpacked folder, make sure it exists")
+		return
+	
+	
+	mod_log("Searching through mods-unpacked/")
 	# loop through it and add all nested folders into a list
 	mods_folder.list_dir_begin()
 
 	var file_name = mods_folder.get_next()
 	var mod_folders = []
-
+	
 	while file_name != "":
 		if mods_folder.current_is_dir():
 			mod_folders.append(file_name)
@@ -25,29 +41,33 @@ func _ready():
 	for mod_path in mod_folders:
 		load_mod(mods_folder_path + "/" + mod_path, mod_path, mods_folder_path)
 	
-
-
-
 	mods_folder_path = OS.get_executable_path().get_base_dir() + "/mods"
 	var mods_packed_folder = DirAccess.open(OS.get_executable_path().get_base_dir() + "/mods")
+	
+	mod_log('')
+	mod_log("Mods folder path: " + mods_folder_path)
+
+	if not mods_packed_folder:
+		mod_log("Failed to open mods folder, make sure it exists")
+		return
 
 	mods_packed_folder.list_dir_begin()
 
 	file_name = mods_packed_folder.get_next()
 	var mod_zips = []
 	
-	print("Starting to look through mods/")
+	mod_log("Searching through mods/")
 	while file_name != "":
-		print(file_name)
+		mod_log(file_name)
 		mod_zips.append(file_name)
-		print(ProjectSettings.load_resource_pack(mods_folder_path + "/" + file_name))
-		print(mods_folder_path + "/" + file_name)
+		mod_log(ProjectSettings.load_resource_pack(mods_folder_path + "/" + file_name))
+		mod_log(mods_folder_path + "/" + file_name)
 
 		file_name = mods_packed_folder.get_next()
 		
 	for mod in mod_zips:
 		var mod_name_wihout_zip = mod.substr(0, len(mod) - 4) 
-		print(mod_name_wihout_zip)
+		mod_log(mod_name_wihout_zip)
 		load_mod("res://" + mod.substr(0, len(mod) - 4), mod_name_wihout_zip, mods_folder_path)
 
 	
@@ -66,16 +86,17 @@ func load_mod(mod_path, mod_name, mods_folder_path):
 	
 	# make sure the mod has a mod_main.gd
 	if not current_mod.file_exists("mod_main.gd"):
-		print("Failed to load " + mod_name + ", no mod_main.gd")
+		mod_log("Failed to load " + mod_name + ", no mod_main.gd")
 		return
 	
 	if not current_mod.file_exists("manifest.json"):
-		print("Failed to load " + mod_name + ", no manifest.json")
+		mod_log("Failed to load " + mod_name + ", no manifest.json")
 		return
 	
 	# load it
 	var current_script = load(mod_path + "/mod_main.gd")
-	print(current_script)
+	mod_log(mod_path + "/mod_main.gd")
+	mod_log(current_script)
 
 	
 	# add it as a child
@@ -84,7 +105,7 @@ func load_mod(mod_path, mod_name, mods_folder_path):
 	
 	# make sure all mod_main.gd scripts have the init function
 	if not new_node.has_method("init"):
-		print("Failed to load " + mod_path + ", no init function")
+		mod_log("Failed to load " + mod_path + ", no init function")
 		new_node.queue_free()
 		return
 
@@ -97,14 +118,14 @@ func load_mod(mod_path, mod_name, mods_folder_path):
 	# check incompatabilites
 	for mod in manifest_dict["incompatabilites"]:
 		if mod in all_mods.keys():
-			print("Failed to load " + mod_name + ", incompatable mod " + mod + " is present")
+			mod_log("Failed to load " + mod_name + ", incompatable mod " + mod + " is present")
 			new_node.queue_free()
 			return
 	
 	# load dependencies
 	for mod in manifest_dict["dependencies"]:
 		if mod not in all_mods.keys():
-			print("Loading dependencies")
+			mod_log("Loading dependencies")
 			load_mod(mods_folder_path + "/" + mod, mod, mods_folder_path)
 
 	all_mods[manifest_dict["namespace"] + "-" + manifest_dict["name"]] = new_node
@@ -114,4 +135,35 @@ func load_mod(mod_path, mod_name, mods_folder_path):
 	self.add_child(new_node)
 
 	new_node.name = manifest_dict["name"]
+
+
+func init_logs():
+	var logs_dir = DirAccess.open("user://logs")
+	
+	
+	if logs_dir.file_exists("mod_loader.log"):
+		var old_log_file = FileAccess.open("user://logs/mod_loader.log", FileAccess.READ)
+		var date_and_time = old_log_file.get_line()
+
+		logs_dir.rename("mod_loader.log", "mod_loader" + date_and_time + ".log")
+
+	var log_file = FileAccess.open("user://logs/mod_loader.log", FileAccess.WRITE)
+
+	log_file.store_line(Time.get_datetime_string_from_system())
+
+
+func mod_log(text, custom_path = ''):
+	text = str(text)
+
+	var log_path = "user://logs/mod_loader.log"
+	var log_file = FileAccess.open("user://logs/mod_loader.log", FileAccess.READ_WRITE)
+	
+	log_file.seek_end()
+	print(text, log_file, "help")
+
+	log_file.store_line(text)
+	log_file = null
+
+	print(text)
+
 
