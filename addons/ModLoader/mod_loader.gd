fc27d6c65360a908793a6ecbd94ba3dd0b42a108
Olvior <olvioro@gmail.com>
2024-08-15 09:33:13 +0200

    Organised the loader a bit

--- addons/ModLoader/mod_loader.gd
+++ addons/ModLoader/mod_loader.gd
@@ -12,20 +12,21 @@ func _ready():
 	
 	mod_log("Starting up the mod loader")
 	mod_log('')
-
-	start_mod_loading()
-
+	
+	start_loading_unpacked()
+	start_loading_packed()
+	
 	get_tree().get_root().print_tree_pretty()
 
 
 
-func start_mod_loading():
+func start_loading_unpacked():
 	# open the mods folder
 	var mods_folder_path = OS.get_executable_path().get_base_dir() + "/mods-unpacked"
 	var mods_folder = DirAccess.open(mods_folder_path)
 	
 	mod_log("Mods unpacked path: " + mods_folder_path)
-
+	
 	if not mods_folder:
 		mod_log("Failed to open mods-unpacked folder, make sure it exists")
 		return
@@ -34,7 +35,7 @@ func start_mod_loading():
 	mod_log("Searching through mods-unpacked/")
 	# loop through it and add all nested folders into a list
 	mods_folder.list_dir_begin()
-
+	
 	var file_name = mods_folder.get_next()
 	var mod_folders = []
 	
@@ -46,21 +47,22 @@ func start_mod_loading():
 	
 	# loop over all the mod folders and add them in
 	for mod_path in mod_folders:
-		var error = load_mod(mods_folder_path + "/" + mod_path, mod_path, mods_folder_path)
-	
-	mods_folder_path = OS.get_executable_path().get_base_dir() + "/mods"
+		var _error = load_mod(mods_folder_path + "/" + mod_path, mod_path, mods_folder_path)
+
+func start_loading_packed():
+	var mods_folder_path = OS.get_executable_path().get_base_dir() + "/mods"
 	var mods_packed_folder = DirAccess.open(mods_folder_path)
 	
 	mod_log('')
 	mod_log("Mods folder path: " + mods_folder_path)
-
+	
 	if not mods_packed_folder:
 		mod_log("Failed to open mods folder, make sure it exists")
 		return
-
+	
 	mods_packed_folder.list_dir_begin()
-
-	file_name = mods_packed_folder.get_next()
+	
+	var file_name = mods_packed_folder.get_next()
 	var mod_zips = []
 	
 	mod_log("Searching through mods/")
@@ -69,16 +71,16 @@ func start_mod_loading():
 		mod_zips.append(file_name)
 		mod_log(ProjectSettings.load_resource_pack(mods_folder_path + "/" + file_name))
 		mod_log(mods_folder_path + "/" + file_name)
-
+	
 		file_name = mods_packed_folder.get_next()
 		
 	for mod in mod_zips:
 		var mod_name_wihout_zip = mod.substr(0, len(mod) - 4) 
 		mod_log(mod_name_wihout_zip)
-		var error = load_mod("res://" + mod.substr(0, len(mod) - 4), mod_name_wihout_zip, mods_folder_path)
-
+		var _error = load_mod("res://" + mod.substr(0, len(mod) - 4), mod_name_wihout_zip, mods_folder_path)
 	
-
+	
+	
 	get_tree().get_root().print_tree_pretty()
 
 
@@ -104,8 +106,8 @@ func load_mod(mod_path, mod_name, mods_folder_path) -> Error:
 	var current_script = load(mod_path + "/mod_main.gd")
 	mod_log(mod_path + "/mod_main.gd")
 	mod_log(current_script)
-
-
+	
+	
 	
 	# add it as a child
 	var new_node = ModNode.new()
@@ -115,9 +117,9 @@ func load_mod(mod_path, mod_name, mods_folder_path) -> Error:
 	if new_node.get_class() != "ModNode":
 		mod_log("Failed to load " + mod_path + ", it does not extend ModNode")
 		mod_log("It extends " + new_node.get_class() + " instead")
-		new_node.queue_free
+		new_node.queue_free()
 		return FAILED
-
+	
 	# load manifest.json
 	var manifest_json = mod_path + "/manifest.json"
 	var manifest_text = FileAccess.get_file_as_string(manifest_json)
@@ -140,17 +142,17 @@ func load_mod(mod_path, mod_name, mods_folder_path) -> Error:
 				return FAILED
 		
 		new_node.dependencies[mod] = all_mods[mod]
-
+	
 	all_mods[manifest_dict["namespace"] + "-" + manifest_dict["name"]] = new_node
-
-
-
+	
+	
+	
 	new_node.name = manifest_dict["namespace"] + "-" + manifest_dict["name"]
 	new_node.name_space = manifest_dict["namespace"]
 	new_node.name_without_namespace = manifest_dict["name"]
-
+	
 	new_node.name_pretty = manifest_dict["name_pretty"]
-
+	
 	new_node.path_to_dir = mod_path
 	
 	self.add_child(new_node)
@@ -167,25 +169,25 @@ func init_logs():
 	if logs_dir.file_exists("mod_loader.log"):
 		var old_log_file = FileAccess.open("user://logs/mod_loader.log", FileAccess.READ)
 		var date_and_time = old_log_file.get_line()
-
+		
 		logs_dir.rename("mod_loader.log", "mod_loader" + date_and_time + ".log")
-
+	
 	var log_file = FileAccess.open("user://logs/mod_loader.log", FileAccess.WRITE)
-
+	
 	log_file.store_line(Time.get_datetime_string_from_system())
 
 
-func mod_log(text, custom_path = ''):
+func mod_log(text):
 	text = str(text)
-
+	
 	var log_path = "user://logs/mod_loader.log"
 	var log_file = FileAccess.open(log_path, FileAccess.READ_WRITE)
 	
 	log_file.seek_end()
-
+	
 	log_file.store_line(text)
 	log_file = null
-
+	
 	print(text)
 
 
