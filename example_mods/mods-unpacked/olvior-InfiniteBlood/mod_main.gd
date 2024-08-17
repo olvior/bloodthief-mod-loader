2c493517ddfd0906c6cd2659a37d7447c163f44c
Olvior <olvioro@gmail.com>
2024-08-17 16:03:39 +0200

    Infinite Blood mod now uses settings

--- example_mods/mods-unpacked/olvior-InfiniteBlood/mod_main.gd
+++ example_mods/mods-unpacked/olvior-InfiniteBlood/mod_main.gd
@@ -1,9 +1,20 @@
 extends "res://addons/ModLoader/mod_node.gd"
 
+var blood_setting = Setting.new("Blood Amount", Setting.SETTING_FLOAT, Vector2(0, 6))
+
+
 func init():
 	ModLoader.mod_log(name_pretty + " mod loaded")
+	
+	settings = {
+		"settings_page_name" = "Infinite Blood",
+		"settings_list" = [
+			blood_setting
+		]
+	}
 
 func _process(_delta):
 	if is_instance_valid(GameManager.player):
-		GameManager.player.blood_amount = 6
-
+		if GameManager.player.blood_amount < blood_setting.value:
+			GameManager.player.blood_amount = blood_setting.value
+		
