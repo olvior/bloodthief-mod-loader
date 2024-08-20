145c5471137236fe67c1a76871944808e3a0f1fe
Olvior <olvioro@gmail.com>
2024-08-20 10:06:39 +0200

    Updated test level

--- example_mods/mods-unpacked/olvior-TestLevel/mod_main.gd
+++ example_mods/mods-unpacked/olvior-TestLevel/mod_main.gd
@@ -3,8 +3,6 @@ extends "res://addons/ModLoader/mod_node.gd"
 
 func init():
 	ModLoader.mod_log(name_pretty + " mod loaded")
-
-
-	ModLoader.mod_log(str(dependencies) + " dep")
-	var level_helper = dependencies["olvior-LevelHelper"]
-	level_helper.create_level("Supercoollevel", 1, path_to_dir + "/testroom.tscn", "Super cool level", "supercoollevelboard", [10, 20, 30], true)
+	
+func _ready():
+	GameManager._level_configs.append(load(path_to_dir + "/proper_config.tres"))
