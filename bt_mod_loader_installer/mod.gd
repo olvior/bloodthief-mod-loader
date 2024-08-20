73d20603e869e279805e7f74833cfd5063ed1366
Olvior <olvioro@gmail.com>
2024-08-20 22:38:03 +0200

    Improved individual mod scene stuff

--- bt_mod_loader_installer/mod.gd
+++ bt_mod_loader_installer/mod.gd
@@ -1,19 +1,42 @@
 extends MarginContainer
 
 var manifest
-var id
-var my_path
-var is_installed
+var id: String
+var my_path: String
+var my_disabled_path: String
+var is_installed: bool
+var is_disabled: bool
 @onready var main = get_tree().get_root().get_child(0)
 
+@onready var install_button: Button = $VBoxContainer/HBoxContainer/install
+@onready var uninstall_button: Button = $VBoxContainer/HBoxContainer/uninstall
+@onready var disable_button: Button = $VBoxContainer/HBoxContainer/disable
+@onready var enable_button: Button = $VBoxContainer/HBoxContainer/enable
+
+var enabled_list: Array[bool] = [true, false, false, true]
+var disabled_list: Array[bool] = [true, false, true, false]
+var uninstalled_list: Array[bool] = [false, true, true, true]
+
 func check_if_installed():
-	var mods_folder = DirAccess.open(main.path + "/mods")
-	my_path = main.path + "/mods/" + id
-	if DirAccess.dir_exists_absolute(my_path):
-		is_installed = true
+	my_path = main.path + "/mods/" + id + ".zip"
+	my_disabled_path = main.path + "/mods/disabled/" + id + ".zip"
 	
-	if is_installed:
-		$VBoxContainer/HBoxContainer/install.text = "Reinstall"
+	if FileAccess.file_exists(my_path):
+		is_installed = true
+		change_buttons(enabled_list)
+	
+	elif FileAccess.file_exists(my_disabled_path):
+		is_disabled = true
+		change_buttons(disabled_list)
+	
+	else:
+		change_buttons(uninstalled_list)
+	
+	
+	
+	if not is_installed:
+		$VBoxContainer/HBoxContainer/disable.disabled = true
+	
 
 func init(new_manifest):
 	manifest = new_manifest
@@ -23,11 +46,7 @@ func init(new_manifest):
 	check_if_installed()
 
 
-func _on_install_button_up():
-	download(manifest["download"], my_path + ".zip")
-
-
-func download(link, path):
+func download(link: String, path: String):
 	var http = HTTPRequest.new()
 	add_child(http)
 	http.connect("request_completed", _http_request_completed)
@@ -40,5 +59,34 @@ func download(link, path):
 func _http_request_completed(result, _response_code, _headers, _body):
 	if result != OK:
 		push_error("Download Failed")
+	else:
+		change_buttons(enabled_list)
+
+
+func change_buttons(button_list: Array[bool]):
+	install_button.disabled = button_list[0]
+	uninstall_button.disabled = button_list[1]
+	disable_button.disabled = button_list[2]
+	enable_button.disabled = button_list[3]
+
+func _on_install_button_up():
+	download(manifest["download"], my_path + ".zip")
+
+func _on_disable_button_up() -> void:
+	DirAccess.rename_absolute(my_path, my_disabled_path)
+	change_buttons(disabled_list)
+	is_disabled = true
+
+func _on_enable_button_up() -> void:
+	DirAccess.rename_absolute(my_disabled_path, my_path)
+	change_buttons(enabled_list)
+	is_disabled = false
+
+func _on_uninstall_button_up() -> void:
+	if is_disabled:
+		DirAccess.remove_absolute(my_disabled_path)
+	else:
+		DirAccess.remove_absolute(my_path)
 	
-	
+	change_buttons(uninstalled_list)
+	is_installed = false
